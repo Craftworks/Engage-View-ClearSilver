7e78d63595a6525d447eff8f5d8c73945e29385c
nakane <ra@dev.rainbowapps.com>
2010-04-06 15:19:20 +0900

    added useful vars for set_data()

--- lib/Engage/WUI/View/ClearSilver.pm
+++ lib/Engage/WUI/View/ClearSilver.pm
@@ -36,9 +36,9 @@ sub process {
     my ( $self, $c ) = @_;
 
     $self->no_wrapper(1) if $c->stash->{'no_wrapper'};
+    $self->set_data( $c );
     $self->set_loadpaths( $c );
     $self->set_template( $c );
-    $self->set_data( $c );
     my $body = $self->render;
     Catalyst::Exception->throw( message => qq/Coudn't render template/ )
         unless defined $body;
@@ -70,7 +70,56 @@ sub set_template {
 
 sub set_data {
     my ( $self, $c ) = @_;
-    $self->data( $c->stash );
+
+    my $stash = $c->stash;
+
+    $stash->{'status'} = $c->res->status;
+
+    my $req = $c->req;
+    my $uri = $req->uri;
+    my $headers = $req->headers;
+    $stash->{'ENV'} = {
+        %ENV,
+        'HTTP_HOST'            => $uri->host_port,
+        'HTTP_REFERER'         => $req->referer || undef,
+        'HTTP_ACCEPT'          => $headers->header('Accept') || undef,
+        'HTTP_ACCEPT_LANGUAGE' => $headers->header('Accept-Language') || undef,
+        'HTTP_ACCEPT_ENCODING' => $headers->header('Accept-Encoding') || undef,
+        'HTTP_USER_AGENT'      => $req->user_agent . '' || undef,
+        'SERVER_PROTOCOL'      => $req->protocol,
+        'PATH_INFO'            => $req->path_info,
+        'QUERY_STRING'         => $uri->query,
+        'REQUEST_METHOD'       => uc $req->method,
+        'REQUEST_URI'          => substr($uri, length($req->base) - 1),
+        'REQUEST_BASE'         => substr($req->base, 0, - 1),
+        'REMOTE_ADDR'          => $req->address,
+    };
+    $stash->{'param'} = $req->params;
+
+    my $action = $c->action;
+    $stash->{'action'} = {
+        'namespace' => $action->namespace,
+        'reverse'   => $action->reverse,
+        'reverse_'  => $action->reverse,
+        'name'      => $action->name,
+    };
+    $stash->{'action'}{'reverse_'} =~ s{/}{_}go;
+
+    my $offset = defined $stash->{'TZ_OFFSET'} ? $stash->{'TZ_OFFSET'} : 0;
+    @{$stash->{'time'}}{qw/
+        year month day hour min sec day_abbr day_name
+        date iso8601 time epoch rfc822 offset tz
+    /} = split /[;]/, POSIX::strftime(
+        "%Y;%m;%d;%H;%M;%S;%a;%A;%Y/%m/%d;%F;%T;%s;%a, %d %b %Y %H:%M:%S %z;%z;%Z",
+    gmtime( time + $offset ));
+
+    if ( $c->can('user_exists') && $c->user_exists ) {
+        $stash->{'user_exists'} = 1;
+        $stash->{'user'} = $c->user->get_object;
+        $stash->{'user'}{'id'} = $c->user->id;
+    }
+
+    $self->data( $stash );
 }
 
 =head1 AUTHOR
